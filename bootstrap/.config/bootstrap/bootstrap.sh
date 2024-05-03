38932320aa5c9507a1a4214016b9ac8bd9b7cee2
Eneko Lakasta <623492+enekid@users.noreply.github.com>
2024-05-03 13:09:13 +0200

    Add more info to bootstrap.sh

--- bootstrap/.config/bootstrap/bootstrap.sh
+++ bootstrap/.config/bootstrap/bootstrap.sh
@@ -4,10 +4,17 @@ sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install neovim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt install neovim
-sudo apt install ripgrep xclip
+sudo apt install make gcc ripgrep unzip git xclip
+sudo snap install nvim --classic
 
 # Install 
 sudo apt install -y build-essential curl git 
+
+# Node
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs latest:20
+asdf global nodejs latest:20
+npm install -g typescript typescript-language-server
+
+# TODO
+# NerdFont
